10aba597180dc8e2b25053d1d32608be99e24a6d
Ben Luty <ben.luty@gmail.com>
2022-10-02 11:15:42 -0500

    Redid old work with MySQL
    MySQL is host on my personal laptop. Need Dr. Y's advice on how to put it on an AWS instance

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import os
 import time
 import io
 
+import mysql.connector
+import mysqlx.helpers
 from pymongo import MongoClient
 from flask import Flask, flash, request, redirect, url_for, send_from_directory, send_file
 from werkzeug.utils import secure_filename
@@ -11,11 +13,32 @@ import gridfs
 # import moto_moto as boto
 # from markupsafe import escape
 
-UPLOAD_FOLDER = '/uploads'
-ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+UPLOAD_FOLDER = '\\uploads\\unclassified'
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
+def classification_to_int(classification_string):
+    if classification_string == "Trash":
+        return 0
+    elif classification_string == "Recycle":
+        return 1
+    elif classification_string == "Compost":
+        return 2
+    else:
+        return -1
+
+
+def mysql_connect():
+    mydb = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="12lego34",
+        database="binit"
+    )
+    return mydb
 
 
 def allowed_file(filename):
@@ -23,44 +46,81 @@ def allowed_file(filename):
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
-@app.route('/uploads', methods=['GET', 'POST'])
-def upload_file():
+@app.route('/', methods=['GET', 'POST'])
+def db_test():
+    db = mysql_connect()
+    cursor = db.cursor()
+
+    sql = "INSERT INTO photos " \
+          "(date, user, machine_classification, path) VALUES " \
+          "(%s, %s, %s, %s)"
+    val = (time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime()), "admin", 1, "C:/uploads/unclassified/photo.jpg")
+    cursor.execute(sql, val)
+
+    #db.commit()
+
+    print(cursor.execute("SELECT * FROM photos"))
+
+    return 'database test'
+
+
+@app.route('/upload/<path:username>', methods=['GET', 'POST'])
+def upload_file(username):
     if request.method == 'POST':
         # check if the post request has the file part
         if 'file' not in request.files:
             flash('No file part')
             return redirect(request.url)
         file = request.files['file']
-        print(file)
         # If the user does not select a file, the browser submits an
         # empty file without a filename.
         if file.filename == '':
             flash('No selected file')
             return redirect(request.url)
         if file and allowed_file(file.filename):
+            timestamp = time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime())
+            file.filename = username + '_' + timestamp + '.' + file.filename.rsplit('.', 1)[1].lower()
             filename = secure_filename(file.filename)
             file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
             file.save(file_path)
 
-            # mongodb here!
-            mongo_client = MongoClient("mongodb+srv://BINITAdmin:iNyp7QoHZ5ReLOSk@binit-cluster.tzibqip.mongodb.net/?retryWrites=true&w=majority")
-            photo_database = mongo_client['Photo-Database']
-            photo_collection = photo_database['Photos']
+            classification = magic_classification_machine.classify(file)
 
-            # gridfs here!
-            fs = gridfs.GridFS(photo_database)
+            db = mysql_connect()
+            cursor = db.cursor()
+            sql = "INSERT INTO photos " \
+                  "(date, user, machine_classification, path, name) VALUES " \
+                  "(%s, %s, %s, %s, %s)"
+            val = (timestamp,
+                   username,
+                   classification_to_int(classification),
+                   file_path,
+                   filename)
+            try:
+                cursor.execute(sql, val)
+            except mysql.connector.errors.IntegrityError:
+                return 'duplicate name'
+            db.commit()
 
-            photo_info = {
-                'time': time.gmtime(),
-                'classification': magic_classification_machine.classify(file),
-                'user': 'unknown',
-                'filename': file.filename,
-                'file-grid': fs.put(open(file_path, 'rb').read())
-            }
+            # # mongodb here!
+            # mongo_client = MongoClient("mongodb+srv://BINITAdmin:iNyp7QoHZ5ReLOSk@binit-cluster.tzibqip.mongodb.net/?retryWrites=true&w=majority")
+            # photo_database = mongo_client['Photo-Database']
+            # photo_collection = photo_database['Photos']
+            #
+            # # gridfs here!
+            # fs = gridfs.GridFS(photo_database)
+            #
+            # photo_info = {
+            #     'time': time.gmtime(),
+            #     'classification': magic_classification_machine.classify(file),
+            #     'user': 'unknown',
+            #     'filename': file.filename,
+            #     'file-grid': fs.put(open(file_path, 'rb').read())
+            # }
+            #
+            # photo_collection.insert_one(photo_info)
 
-            photo_collection.insert_one(photo_info)
-
-            return redirect(url_for('upload_file', name=filename))
+            return classification
     return '''
     <!doctype html>
     <title>Upload new File</title>
@@ -72,25 +132,51 @@ def upload_file():
     '''
 
 
-@app.route('/uploads/<path:filename>', methods=['GET', 'POST'])
-def download(filename):
-    # return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
-    # mongodb here!
-    mongo_client = MongoClient(
-        "mongodb+srv://BINITAdmin:iNyp7QoHZ5ReLOSk@binit-cluster.tzibqip.mongodb.net/?retryWrites=true&w=majority")
-    photo_database = mongo_client['Photo-Database']
-    photo_collection = photo_database['Photos']
+@app.route('/list_unclassified/<path:username>', methods=['GET'])
+def list_unclassified(username):
 
-    fs = gridfs.GridFS(photo_database)
+    db = mysql_connect()
+    cursor = db.cursor()
+    sql = "SELECT id, name FROM photos WHERE user='" + username + "' AND user_classification=-1"
+    cursor.execute(sql)
+    paths = cursor.fetchall()
+    # print(paths)
+    return paths
 
-    file_grid = photo_collection.find({"filename": filename}, {"file-grid": 1})
 
-    # print(file_grid.next())
+@app.route('/download_by_name/<path:filename>', methods=['GET'])
+def download_by_path(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
-    if file_grid is None:
-        return None
 
-    raw_image = io.BytesIO(fs.get(file_grid.next().get('file-grid')).read())
+@app.route('/download_by_id/<path:photo_id>', methods=['GET'])
+def download_by_id(photo_id):
+    db = mysql_connect()
+    cursor = db.cursor()
+    sql = "SELECT name FROM photos WHERE id=" +  photo_id
+    cursor.execute(sql)
+    filename = cursor.fetchone()[0]
+    print(filename)
+    if filename is None:
+        filename = ""
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
-    return send_file(raw_image, download_name='image.jpg', as_attachment=True)
+    # # mongodb here!
+    # mongo_client = MongoClient(
+    #     "mongodb+srv://BINITAdmin:iNyp7QoHZ5ReLOSk@binit-cluster.tzibqip.mongodb.net/?retryWrites=true&w=majority")
+    # photo_database = mongo_client['Photo-Database']
+    # photo_collection = photo_database['Photos']
+    #
+    # fs = gridfs.GridFS(photo_database)
+    #
+    # file_grid = photo_collection.find({"filename": filename}, {"file-grid": 1})
+    #
+    # # print(file_grid.next())
+    #
+    # if file_grid is None:
+    #     return None
+    #
+    # raw_image = io.BytesIO(fs.get(file_grid.next().get('file-grid')).read())
+    #
+    # return send_file(raw_image, download_name='image.jpg', as_attachment=True)
 
