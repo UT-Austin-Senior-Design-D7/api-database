177729386d59f5e9d869d966fcda02b6a72ae3e9
Ben Luty <ben.luty@gmail.com>
2022-11-10 10:22:10 -0600

    e

--- app.py
+++ app.py
@@ -119,11 +119,7 @@ def upload_file(device_id):
             # process = subprocess.run(['echo', 'the pain train'], stdout=subprocess.PIPE, universal_newlines=True)
             # print(process)
 
-            cmd = ["python3",
-                   "/home/ubuntu/RecycleNet/webcam.py",
-                   "--resume /home/ubuntu/RecycleNet/save/model_best.pth.tar",
-                   "--save_dir " + file_path,
-                   "--resize_needed True"]
+            cmd = ["python3 /home/ubuntu/RecycleNet/webcam.py --resume /home/ubuntu/RecycleNet/save/model_best.pth.tar --save_dir " + file_path + " --resize_needed True"]
             process = subprocess.run(cmd, shell=True, capture_output=True)
 
             process_output = process.stdout
